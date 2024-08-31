9112eb723411076dae081196cddd94935d91c0d5
turhanomer <omerturhan16@outlook.com>
2024-08-31 11:26:25 +0300

    2666

---
+++ Function Transformations/allow-one-function-call.js
@@ -0,0 +1,19 @@
+var once = function (fn) {
+  let called = false;
+  let result;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      result = fn (...args);
+      return result;
+    }
+    return undefined;
+  }
+}
+
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+
+console.log(onceFn(1, 2, 3)); // Output: 6
+console.log(onceFn(2, 3, 6)); // Output: undefined
