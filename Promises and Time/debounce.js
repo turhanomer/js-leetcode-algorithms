3172b5b7378786678321803034a2a534fb3199a3
turhanomer <omerturhan16@outlook.com>
2024-09-08 15:50:29 +0300

    2627

---
+++ Promises and Time/debounce.js
@@ -0,0 +1,12 @@
+var debounce = function (fn, t) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), t);
+  };
+};
+
+const log = debounce(console.log, 100);
+log("Hello"); // cancelled
+log("Hello"); // cancelled
+log("Hello"); // Logged at t=100ms
